6ba32c0abc9d8806fca7c7286c1ed853432b89a6
Piramol Krishnan <piramolkk@gmail.com>
2022-09-11 17:47:57 +0800

    CREATE: ParticleData class, inheriting from HouseInfo, overriding methods

---
+++ sensor/particle_count_info.py
@@ -0,0 +1,33 @@
+from curses.textpad import rectangle
+from house_info import HouseInfo
+from datetime import date, datetime
+class ParticleData(HouseInfo):
+    def _convert_data(self,data):
+        recs = []
+
+        for rec in data:
+            recs.append(float(rec))
+        
+        return recs
+    
+    def get_data_by_area(self, rec_area=0):
+        recs = super().get_data_by_area("particulate", rec_area)
+        return self._convert_data(recs)
+    
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("particulate", rec_date)
+        return self._convert_data(recs)
+    
+    def get_data_concentrations(self,data):
+        particulate = {"good":int(0),
+                        "moderate":int(0),
+                        "bad":int(0)}
+        print(type(particulate["good"]))
+        for rec in data:
+            if(rec < 50.0):
+                particulate["good"] += 1
+            elif(rec>50.0 and rec <=100.0):
+                particulate["moderate"] += 1
+            elif(rec>100.0):
+                particulate["bad"]+=1
+        return particulate
